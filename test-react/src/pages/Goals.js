e0a41b2dde89e65bd87da1f1c00a66c156235038
ReactExpert <script.masterwebdeveloper@gmail.com>
2018-09-26 22:24:23 +0800

    upload

---
+++ test-react/src/pages/Goals.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Navbar from '../components/Navbar';
+import Jumbotron from '../components/Jumbotron';
+
+
+class Goals extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <Jumbotron />
+            </div>
+        );
+    }
+}
+
+export default Goals;
---
+++ test-react/src/pages/Influencers.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Navbar from '../components/Navbar';
+import Jumbotron from '../components/Jumbotron';
+
+
+class Influencers extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <Jumbotron />
+            </div>
+        );
+    }
+}
+
+export default Influencers;
