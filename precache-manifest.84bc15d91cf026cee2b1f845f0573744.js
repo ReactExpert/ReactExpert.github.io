448b3b0c168fe722ce38ec6571917628757c2356
ReactExpert <38716557+ReactExpert@users.noreply.github.com>
2018-10-16 11:30:27 +0800

    test project

---
+++ precache-manifest.84bc15d91cf026cee2b1f845f0573744.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9f9bc1cc3e62ebf4c3aa",
+    "url": "/static/js/main.9f9bc1cc.chunk.js"
+  },
+  {
+    "revision": "1ace436bc362541eeac0",
+    "url": "/static/js/1.1ace436b.chunk.js"
+  },
+  {
+    "revision": "9f9bc1cc3e62ebf4c3aa",
+    "url": "/static/css/main.ac734901.chunk.css"
+  },
+  {
+    "revision": "b4b3792e6feb762a719a2c2df91ca239",
+    "url": "/index.html"
+  }
+];
